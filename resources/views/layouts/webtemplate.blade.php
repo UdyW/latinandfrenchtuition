ba2eb00a00dd731fc371e0d1921b3a43e3a94078
Udyw <udayanthaw@yahoo.com>
2020-03-12 17:29:26 -0500

    optimizing

--- resources/views/layouts/webtemplate.blade.php
+++ resources/views/layouts/webtemplate.blade.php
@@ -17,20 +17,20 @@
     <link rel="stylesheet" href="../assets/slick/slick.css">
     <link rel="stylesheet" href="../assets/slick/slick-theme.css">
     
-    <script rel="preload" src="{{ asset('material') }}/js/core/jquery.min.js" as="script"></script>
-    <script rel="preload" src="{{ asset('material') }}/js/core/popper.min.js" as="script"></script>
-    <script rel="preload" src="{{ asset('material') }}/js/core/bootstrap-material-design.min.js" as="script"></script>
-    <script rel="preload" src="{{ asset('material') }}/js/plugins/perfect-scrollbar.jquery.min.js" as="script"></script>
+    <script rel="preload" src="{{ asset('material') }}/js/core/jquery.min.js" as="script" defer></script>
+    <script rel="preload" src="{{ asset('material') }}/js/core/popper.min.js" as="script" defer></script>
+    <script rel="preload" src="{{ asset('material') }}/js/core/bootstrap-material-design.min.js" as="script" defer></script>
+    <script rel="preload" src="{{ asset('material') }}/js/plugins/perfect-scrollbar.jquery.min.js" as="script" defer></script>
 
     {{--<script src="assets/js/jquery.js"></script>--}}
-    <script rel="preload"  src="assets/slick/slick.js" as="script"></script>
-    <script  rel="preload" src="assets/js/bootstrap.min.js" as="script"></script>
-    <script rel="preload"  src="assets/js/main.js" as="script"></script>
+    <script rel="preload"  src="assets/slick/slick.js" as="script" defer></script>
+    <script  rel="preload" src="assets/js/bootstrap.min.js" as="script" defer></script>
+    <script rel="preload"  src="assets/js/main.js" as="script" defer></script>
 
-    <script rel="preload"  src="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.js" as="script"></script>
-    <script rel="preload"  src="//code.jquery.com/ui/1.12.1/jquery-ui.js" as="script"></script>
-    <script rel="preload"  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.js" as="script"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.css"  as="script"/>
+    <script rel="preload"  src="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.js" as="script" defer></script>
+    <script rel="preload"  src="//code.jquery.com/ui/1.12.1/jquery-ui.js" as="script" defer></script>
+    <script rel="preload"  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.js" as="script" defer></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.css"/>
     <title>Online Tuition</title>
 </head>
 <body class="{{ $class ?? '' }}">
